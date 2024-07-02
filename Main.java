332ebc738425e11c1570b9aff5811538dcf1e614
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-07-02 13:58:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -46,9 +46,9 @@ public class Main {
         int num3 = input.nextInt();
 
         if (num1 > num2  && num1 > num3) {
-            System.out.println(num1);
+            System.out.println("greatest:"+num1);
         } else if (num2 > num1 && num2 > num3) {
-            System.out.println(num2);
+            System.out.println("greatest:"+num2);
         } else {
             System.out.print("greatest:"+num3);
         }
